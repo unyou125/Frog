ec5ef585d15e65d56976c1fe0c026758a1f02eef
unyou125 <144504949+unyou125@users.noreply.github.com>
2024-09-10 21:25:09 -0700

    Create js.js

---
+++ js.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Redirecting...</title>
+    <script type="text/javascript">
+        window.location.href = "https://discord.gg/5QHKcJvS";
+    </script>
+</head>
+<body>
+    <p>If you are not redirected automatically, follow this <a href="https://discord.gg/5QHKcJvS">link to Discord</a>.</p>
+</body>
+</html>
